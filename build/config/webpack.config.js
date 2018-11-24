df0f4d1ab9aeda27859b1673f1e4f28f2cdfba41
Matthew Laird <hvk500@gmail.com>
2018-11-24 17:46:22 +0200

    -- Getting CI working again --

--- build/config/webpack.config.js
+++ build/config/webpack.config.js
@@ -1,11 +1,11 @@
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const buildPaths = require('./paths');
 const buildOptions = require('./options');
 
 module.exports = {
-  cache: buildOptions.cache.enabled,
+  // cache: buildOptions.cache.enabled,
   mode: buildOptions.buildStage,
   context: buildPaths.input.baseDir,
   entry: buildPaths.input.chunks,
@@ -15,13 +15,13 @@ module.exports = {
   },
   plugins: [
     new webpack.ProgressPlugin(),
-    new HardSourceWebpackPlugin({
-      cacheDirectory: buildPaths.output.cacheDir,
-      cachePrune: buildOptions.cache.lifeTime
-    }),
-    new CleanWebpackPlugin(buildPaths.clean.include, {
-        root: buildPaths.clean.root
-    }),
+    // new HardSourceWebpackPlugin({
+    //   cacheDirectory: buildPaths.output.cacheDir,
+    //   cachePrune: buildOptions.cache.lifeTime
+    // }),
+    // new CleanWebpackPlugin(buildPaths.clean.include, {
+    //     root: buildPaths.clean.root
+    // }),
   ],
   module: {
     rules: [
